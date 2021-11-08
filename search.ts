205725b93a90767c4995b60ed4961449dac3e3a9
moncefplastin07 <moncefplastin07@gmail.com>
2021-11-08 16:30:37 +0100

    deploy

--- search.ts
+++ search.ts
@@ -8,16 +8,11 @@ export const searchInDB = async (query: any = "", db = "" ) => {
     th: "./db/thakafa.json"
   }[db]
   if (dbPath == null) {
-    return {
-      errorCode: 404,
-    }
+    return []
   }
   const db_ = JSON.parse(await Deno.readTextFile(dbPath));
   const searchResult = db_.filter((book: any) =>
     book.title?.search(query) > 0 || book.author?.search(query)  > 0 || book.ID?.startsWith(query) 
     );
-    return searchResult.length>0 ? { sourceCode: 200, searchResult }
-    : {
-      errorCode: 404,
-    };
+    return searchResult
 };
