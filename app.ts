a3c4322a4ba95fda723729da44812b3a677616cf
moncefplastin07 <moncefplastin07@gmail.com>
2022-03-01 10:02:36 +0100

    refactor: rename varable

--- app.ts
+++ app.ts
@@ -3,16 +3,16 @@ import { searchInDB } from "./search.ts"
 const { args, listen, serveHttp } = Deno;
 const port = parseArgs(args).port;
 const listener = listen({ port });
-console.log(`http://localhost:${port}/`);
+
 
 for await (const conn of listener) {
   (async () => {
     const requests = serveHttp(conn);
     for await (const { respondWith, request } of requests) {
-      const reauestURL = new URL(request.url)
-      const searchQuery = reauestURL.searchParams.get("q")
-      const dbs = reauestURL.searchParams.get("dbs") as string
-      console.log(dbs)
+      const requestURL = new URL(request.url)
+      const searchQuery = requestURL.searchParams.get("q")
+      const dbs = requestURL.searchParams.get("dbs") as string
+
       respondWith(
         
         new Response(JSON.stringify(await searchInDB(searchQuery, dbs)), {
